9607dc1ac5f0dc921c9027f8edb0b0a9afe98272
szTheory <szTheory@users.noreply.github.com>
2022-11-03 13:47:36 -0400

    Enemy spawn trigger volume, enemy spawn volume, enemy spawners

---
+++ Assets/Scripts/Enemy/EnemySpawnVolume.cs
@@ -0,0 +1,55 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnVolume : MonoBehaviour
+{
+    // Array of enemy spawners in the game
+    private EnemySpawner[] enemySpawners;
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        // Get array of enemy spawners in the game
+        enemySpawners = FindObjectsOfType<EnemySpawner>();
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+    
+    // Search for all enemy spawner's inside the volume boundaries within the world
+    public void SpawnEnemies()
+    {
+        // Loop through all the enemy spawners
+        foreach (var enemySpawner in enemySpawners)
+        {
+            // Check if the enemy spawner is inside the volume
+            if (IsInsideVolume(enemySpawner.transform.position))
+            {
+                // Spawn the enemy
+                enemySpawner.SpawnEnemy();
+            }
+            
+            bool IsInsideVolume(Vector3 transformPosition)
+            {
+                // Check if the position is within the volume
+                return transformPosition.x >= transform.position.x - transform.localScale.x / 2 &&
+                       transformPosition.x <= transform.position.x + transform.localScale.x / 2 &&
+                       transformPosition.y >= transform.position.y - transform.localScale.y / 2 &&
+                       transformPosition.y <= transform.position.y + transform.localScale.y / 2 &&
+                       transformPosition.z >= transform.position.z - transform.localScale.z / 2 &&
+                       transformPosition.z <= transform.position.z + transform.localScale.z / 2;
+            }
+        }
+    }
+    
+    // Draw a box in the editor to show where the enemy spawn volume is
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position, transform.localScale);
+    }
+}
--- Assets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -1,21 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
     // The enemy prefab to spawn with this enemy spawner
     [SerializeField]
-    private GameObject enemyPrefab;
+    private Enemy enemyPrefab;
     
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         
     }
@@ -23,7 +21,18 @@ public class EnemySpawner : MonoBehaviour
     // Spawn an enemy
     public void SpawnEnemy()
     {
-        // Instantiate the enemy prefab
+        // Spawn the enemy prefab
         Instantiate(enemyPrefab, transform.position, transform.rotation);
+    }
+    
+    // Draw a gizmo representing the spawn point that shows a symbol for the enemy that will be spawned
+    private void OnDrawGizmos()
+    {
+        // Set the color of the gizmo to green
+        Gizmos.color = Color.green;
+        
+        // Draw the enemy's mesh to represent the enemy in editor
+        var mesh = enemyPrefab.GetComponent<MeshFilter>().sharedMesh; 
+        Gizmos.DrawMesh(mesh, 0, transform.position, transform.rotation, transform.localScale);
     }
 }
