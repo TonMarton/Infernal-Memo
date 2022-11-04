081c7aebe78973e366042796bf48309de452905e
szTheory <szTheory@users.noreply.github.com>
2022-11-04 19:44:37 -0400

    Pause menu WIP. Add shotgun to project

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    // hold a reference to continue, main menu, and exit buttons
+    [SerializeField] private GameObject continueButton;
+    [SerializeField] private GameObject mainMenuButton;
+    [SerializeField] private GameObject exitButton;
+
+    private void Awake()
+    {
+    }
+
+    public void Activate()
+    {
+        // set the buttons to be active
+        continueButton.SetActive(true);
+        mainMenuButton.SetActive(true);
+        exitButton.SetActive(true);
+    }
+
+    public void Close()
+    {
+        // set the buttons to be inactive
+        continueButton.SetActive(false);
+        mainMenuButton.SetActive(false);
+        exitButton.SetActive(false);
+    }
+}
