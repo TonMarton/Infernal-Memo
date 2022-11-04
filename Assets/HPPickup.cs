73d755c244b59d99d5b6b24646fd87638a98e373
Márton Nagy <ton.marton@gmail.com>
2022-11-04 17:55:21 -0400

    fix: make heal logic more concise

--- Assets/HPPickup.cs
+++ Assets/HPPickup.cs
@@ -1,15 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class HPPickup : MonoBehaviour
 {
     [SerializeField]
-    int HP = 10;
+    private int HP = 10;
 
     private void OnTriggerEnter(Collider other)
     {
         PlayerStats playerStats = other.GetComponent<PlayerStats>();
+
         if (playerStats != null) {
             playerStats.Heal(HP);
             gameObject.SetActive(false);
