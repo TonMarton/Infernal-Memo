924ef18bfc130ab4905904c086fe3baeb4521957
szTheory <szTheory@users.noreply.github.com>
2022-11-04 17:36:14 -0400

    Player HUD

---
+++ Assets/Scripts/UI/HUD.cs
@@ -0,0 +1,29 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class HUD : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI healthText;
+    [SerializeField] private TextMeshProUGUI armorText;
+    [SerializeField] private TextMeshProUGUI shellsText;
+
+    private PlayerStats playerStats;
+
+    private void Awake()
+    {
+        // hide cursor
+        Cursor.visible = false;
+
+        // store a reference to the player's PlayerStats component
+        var player = GameObject.Find("Player");
+        playerStats = player.GetComponent<PlayerStats>();
+    }
+
+    private void Update()
+    {
+        // update the HUD with the player's stats
+        healthText.text = "Health: " + playerStats.health;
+        armorText.text = "Armor: " + playerStats.armor;
+        shellsText.text = "Shells: " + playerStats.shells;
+    }
+}
