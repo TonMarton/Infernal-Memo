e6617d1d0c385ba6815a45c25601d228896742da
szTheory <szTheory@users.noreply.github.com>
2022-11-05 10:53:17 -0400

    new input system hooked up. pause menu working

---
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    [SerializeField] private PauseMenu pauseMenu;
+
+    private void Awake()
+    {
+        // Set up the player input actions
+        var inputActions = new InputActions();
+        inputActions.Player.Enable();
+        inputActions.Player.Pause.performed += _ctx => pauseMenu.Toggle();
+    }
+}
