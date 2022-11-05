c48997352e23f8b7ed4e81f11b7ddbd8ba803a4c
szTheory <szTheory@users.noreply.github.com>
2022-11-05 12:37:40 -0400

    Stapler melee weapon system WIP

--- Assets/Scripts/Enemy/EnemyStats.cs
+++ Assets/Scripts/Enemy/EnemyStats.cs
@@ -5,13 +5,43 @@ using UnityEngine;
 public class EnemyStats : MonoBehaviour
 {
     [SerializeField] private int startHealth = 100;
-
-    private int health;
+    [SerializeField] private float health;
 
     // Start is called before the first frame update
     private void Awake()
     {
         health = startHealth;
+    }
+
+    // take damage
+    public void TakeDamage(float damage)
+    {
+        // take damage
+        health -= damage;
+        
+        Debug.Log("Enemy took " + damage + " damage. Health is now " + health);
+
+        // should die?
+        if (health <= 0)
+        {
+            // die
+            Die();
+        }
+        else
+        {
+            // TOD: play take damage sound with fmod
+        }
+    }
+
+    // die
+    private void Die()
+    {
+        // TODO: play death sound with fmod
+        
+        Debug.Log("Enemy died");
+
+        // disable enemy
+        gameObject.SetActive(false);
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/Player/PlayerWeaponSystem.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using UnityEngine;
+
+// enum for melee and shotgun weapons
+public enum WeaponType
+{
+    Stapler,
+    Shotgun
+}
+
+public class PlayerWeaponSystem : MonoBehaviour
+{
+    private WeaponType currentWeaponType;
+    private PlayerStaplerAttack staplerAttack;
+    
+    private void Awake()
+    {
+        // set reference to stapler attack
+        staplerAttack = GetComponent<PlayerStaplerAttack>();
+        
+        // start with melee
+        currentWeaponType = WeaponType.Stapler;
+    }
+    
+    // attack
+    public void Attack()
+    {
+        switch (currentWeaponType)
+        {
+            case WeaponType.Stapler:
+                // attack with melee
+                staplerAttack.Attack();
+                break;
+            case WeaponType.Shotgun:
+                // attack with shotgun
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+    
+    public void SwitchWeapon(WeaponType weaponType)
+    {
+        // set current weapon
+        currentWeaponType = weaponType;
+    }
+    
+    public void SwitchWeaponNext()
+    {
+        // switch to next weapon
+        currentWeaponType = (WeaponType)(((int)currentWeaponType + 1) % System.Enum.GetValues(typeof(WeaponType)).Length);
+    }
+    
+    public void SwitchWeaponPrevious()
+    {
+        // switch to previous weapon
+        currentWeaponType = (WeaponType)(((int)currentWeaponType - 1) % System.Enum.GetValues(typeof(WeaponType)).Length);
+    }
+}
