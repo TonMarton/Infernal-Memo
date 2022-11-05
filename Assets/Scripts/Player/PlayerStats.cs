d9dec2364598ac5f2e40cf2e7ea2793e40096b9b
szTheory <szTheory@users.noreply.github.com>
2022-11-05 10:53:17 -0400

    new input system hooked up. pause menu working

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -4,40 +4,39 @@ using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
 {
-    [SerializeField]
-    private int startingHealth = 100;
+    [SerializeField] private int startingHealth = 100;
 
-    [SerializeField]
-    private int maxHealth = 100;
+    [SerializeField] private int maxHealth = 100;
 
     public int health { get; private set; }
     public int armor { get; private set; }
     public int shells { get; private set; } = 0;
-    
+
     // Start is called before the first frame update
     private void Awake()
     {
         // Initialize health 
         health = startingHealth;
     }
-    
+
     public void TakeDamage(int damage)
     {
         health -= damage;
-        
+
         // Print how much damage was taken, and the player's current health
         Debug.Log("Took " + damage + " damage. Current health: " + health);
-        
+
         if (health <= 0)
         {
             Die();
             return;
         }
-        
+
         // TODO: play hurt sound with Fmod
     }
 
-    public void Heal(int healing) {
+    public void Heal(int healing)
+    {
         health = Mathf.Min(healing + health, maxHealth);
 
         Debug.Log("Healed " + healing + " damage. Current health: " + health);
@@ -51,6 +50,5 @@ public class PlayerStats : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        
     }
 }
--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -1,31 +1,50 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
     // hold a reference to continue, main menu, and exit buttons
-    [SerializeField] private GameObject continueButton;
-    [SerializeField] private GameObject mainMenuButton;
-    [SerializeField] private GameObject exitButton;
+    [SerializeField] private Button continueButton;
+    [SerializeField] private Button mainMenuButton;
+    [SerializeField] private Button exitButton;
 
-    private void Awake()
+    private void Start()
     {
-        // bind escape to open pause menu
-        // InputManager.Instance.BindAction("Pause", OpenPauseMenu, InputManager.INPUT_STATE.PRESSED);
+        // continue button toggles the pause menu
+        continueButton.onClick.AddListener(Toggle);
+
+        // exit button quits the game 
+        exitButton.onClick.AddListener(Quit);
     }
 
-    public void Activate()
+    // toggle the pause menu
+    public void Toggle()
     {
-        // set the buttons to be active
-        continueButton.SetActive(true);
-        mainMenuButton.SetActive(true);
-        exitButton.SetActive(true);
+        // toggle the pause menu 
+        gameObject.SetActive(!gameObject.activeSelf);
+
+        // if pause menu is active, show the mouse cursor
+        if (gameObject.activeSelf)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+        // otherwise hide the mouse cursor
+        else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
     }
 
-    public void Close()
+    private static void Quit()
     {
-        // set the buttons to be inactive
-        continueButton.SetActive(false);
-        mainMenuButton.SetActive(false);
-        exitButton.SetActive(false);
+#if UNITY_EDITOR
+        // Application.Quit() does not work in the editor so
+        // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+         Application.Quit();
+#endif
     }
 }
