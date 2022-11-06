d01b9a0816724b2b1bc52ae030bb3506764f4a5c
szTheory <szTheory@users.noreply.github.com>
2022-11-05 20:54:20 -0400

    weapon system with shotgun wip

--- Assets/Scripts/Player/PlayerWeaponSystem.cs
+++ Assets/Scripts/Player/PlayerWeaponSystem.cs
@@ -1,3 +1,72 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:97f76da4387f12e529e4a43fbd4d638635a0ad14a3176979260c9edf85bd0957
-size 1652
+﻿using System;
+using UnityEngine;
+
+// enum for melee and shotgun weapons
+public enum WeaponType
+{
+    Stapler,
+    Shotgun
+}
+
+[DisallowMultipleComponent]
+public class PlayerWeaponSystem : MonoBehaviour
+{
+
+    [SerializeField] private HUD HUD;
+
+    private WeaponType currentWeaponType;
+    private PlayerStaplerAttack staplerAttack;
+    private Shotgun shotgun;
+    
+    private void Awake()
+    {
+        // set reference to stapler attack
+        staplerAttack = GetComponent<PlayerStaplerAttack>();
+        
+        // set reference to shotgun 
+        shotgun = GetComponentInChildren<Shotgun>();
+        
+        // start with melee
+        currentWeaponType = WeaponType.Stapler;
+    }
+    
+    // attack
+    public void Attack()
+    {
+        switch (currentWeaponType)
+        {
+            case WeaponType.Stapler:
+                staplerAttack.Attack();
+                break;
+            case WeaponType.Shotgun:
+                shotgun.Shoot();
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+    
+    public void SwitchWeapon(WeaponType weaponType)
+    {
+        // log the weapon we switched to
+        Debug.Log("Switched to " + weaponType);
+        
+        // set current weapon
+        currentWeaponType = weaponType;
+
+        // show crosshair for shotgun
+        HUD.ToggleCrossHair(weaponType == WeaponType.Shotgun);
+    }
+    
+    public void SwitchWeaponNext()
+    {
+        // switch to next weapon
+        SwitchWeapon((WeaponType)(((int)currentWeaponType + 1) % System.Enum.GetValues(typeof(WeaponType)).Length));
+    }
+    
+    public void SwitchWeaponPrevious()
+    {
+        // switch to previous weapon
+        SwitchWeapon((WeaponType)(((int)currentWeaponType - 1) % System.Enum.GetValues(typeof(WeaponType)).Length));
+    }
+}
