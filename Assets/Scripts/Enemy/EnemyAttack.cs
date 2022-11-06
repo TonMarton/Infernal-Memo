c19037bd468309b47acaa11a740e2807ce7556c7
szTheory <szTheory@users.noreply.github.com>
2022-11-05 22:59:29 -0400

    Shotgun damages enemy (but only first time, a bug). Also plays sound in theory but I don't hear them

--- Assets/Scripts/Enemy/EnemyAttack.cs
+++ Assets/Scripts/Enemy/EnemyAttack.cs
@@ -13,11 +13,15 @@ public class EnemyAttack : MonoBehaviour
     // damage for the attack
     [SerializeField] protected int damage = 10;
 
+    // Sounds
+    private FMOD.Studio.EventInstance attackSoundInstance;
+
     public AttackState attackState { get; protected set; }
 
     public virtual void Attack()
     {
-        // TODO: play attack sound with Fmod
+        // play hurt sound
+        SoundUtils.PlaySound3D(attackSoundInstance, "Sfxs/Enemy/Enemy attack", gameObject);
     }
 
     public void NoLongerAttacking()
--- Assets/Scripts/Enemy/EnemyStats.cs
+++ Assets/Scripts/Enemy/EnemyStats.cs
@@ -5,16 +5,29 @@ public class EnemyStats : MonoBehaviour
 {
     [SerializeField] private int startHealth = 100;
     [SerializeField] private float health;
+    [SerializeField] private float minTimeBetweenDamageSounds = 0.5f;
+
+    // Sounds
+    private FMOD.Studio.EventInstance hurtSoundInstance;
+    private FMOD.Studio.EventInstance deathSoundInstance;
+
     private CapsuleCollider capsuleCollider;
+    private float damageSoundCooldown;
 
     // Start is called before the first frame update
     private void Awake()
     {
         // get a reference to the child capsule collider
         capsuleCollider = GetComponentInChildren<CapsuleCollider>();
-        
+
         // set starting health
         health = startHealth;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        damageSoundCooldown -= Time.deltaTime;
     }
 
     // take damage
@@ -23,13 +36,15 @@ public class EnemyStats : MonoBehaviour
         // take damage
         health -= damage;
         
-        Debug.Log("~~~ Enemy took " + damage + " damage. Health is now " + health);
-        
+        // log damage
+        Debug.Log("Took " + damage + " damage. Health is now " + health);
+
         // apply knockback if it was provided
         if (knockback != null)
         {
             // apply knockback to the enemy with the given vector and force
-            capsuleCollider.GetComponent<Rigidbody>().AddForce(knockback.Value.direction * knockback.Value.force, ForceMode.Impulse);
+            capsuleCollider.GetComponent<Rigidbody>()
+                .AddForce(knockback.Value.direction * knockback.Value.force, ForceMode.Impulse);
         }
 
         // should die?
@@ -40,7 +55,17 @@ public class EnemyStats : MonoBehaviour
         }
         else
         {
-            // TOD: play take damage sound with fmod
+            // has it been long enough since the last damage sound?
+            if (damageSoundCooldown <= 0)
+            {
+                // log playing damage sound
+                Debug.Log("Playing damage sound");
+                // play hurt sound
+                SoundUtils.PlaySound3D(hurtSoundInstance, "Sfxs/Enemy/Enemy Damage", gameObject);
+                
+                // reset the damage sound cooldown
+                damageSoundCooldown = minTimeBetweenDamageSounds;
+            }
         }
     }
 
@@ -48,15 +73,11 @@ public class EnemyStats : MonoBehaviour
     private void Die()
     {
         // TODO: play death sound with fmod
-        
-        Debug.Log("---- Enemy died");
+
+        // play death sound
+        SoundUtils.PlaySound3D(hurtSoundInstance, "Sfxs/Enemy/Enemy death", gameObject);
 
         // disable enemy
         gameObject.SetActive(false);
-    }
-
-    // Update is called once per frame
-    private void Update()
-    {
     }
 }
